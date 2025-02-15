cfda1f699bbee20173ba43b269476eb05de58ad6
unknown <maxammadjonovoyatillo1@gmail.com>
2025-02-16 02:28:20 +0500

    start recipe

--- common/views.py
+++ common/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'index.html')
+
+def about_page(request):
+    return render(request, 'about.html')
+
+def phone_number_page(request):
+    return render(request, 'phone number.html')
+
+def wife_page(request):
+    return render(request, 'my_wife.html')
