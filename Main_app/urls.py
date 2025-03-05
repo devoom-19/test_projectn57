f9bb2ef19cf9075c598134a5411665a0431bd1e5
unknown <maxammadjonovoyatillo1@gmail.com>
2025-03-05 14:46:28 +0500

    template, HTMLs

--- Main_app/urls.py
+++ Main_app/urls.py
@@ -1,11 +1,14 @@
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from Main_app import settings
+from common.views import home_page, about_page, author_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('about/', about_page),
+    path('author/', author_page)
 ]
 
 if settings.DEBUG:
--- common/views.py
+++ common/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
+
 def home_page(request):
     return render(request, 'index.html')
 
 def about_page(request):
-    return render(request, 'about.html')
+    return render(request, 'pages/about.html')
 
-def phone_number_page(request):
-    return render(request, 'phone number.html')
-
-def wife_page(request):
-    return render(request, 'my_wife.html')
+def author_page(request):
+    return render(request, 'pages/author.html')
